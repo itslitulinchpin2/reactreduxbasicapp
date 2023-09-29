9a968955c82c0a943cf7c4e5d107c349dbd456c7
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-09-29 04:26:40 +0200

    css form setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 //import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
+function Left1(){
+  return(
+    <div>
+      <h1>Left1</h1>
+      <Left2></Left2>
+    </div>
+  )
+}
+
+function Left2(){
+  return(
+    <div>
+      <h1>Left2</h1>
+      <Left3></Left3>
+    </div>
+  )
+}
+
+function Left3(){
+  return(
+    <div>
+      <h1>Left3</h1>
+    </div>
+  )
+}
+
 
 function App() {
+  const [number,setNumber]=useState(1);
   return (
-    <div>
-      commit test
+    <div id="container">
+      <h1>Root</h1>
+      <Left1></Left1>
     </div>
   );
 }
